3d72ae62517093aae813de405c76a421c84fe857
lsg0862 <100072451+dml113@users.noreply.github.com>
2024-03-19 15:54:06 +0900

    Update JavaApplication.java

--- java/JavaApplication.java
+++ java/JavaApplication.java
@@ -16,8 +16,8 @@ public class DemoApplication {
 @RestController
 class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    @GetMapping("/health")
+    public String health() {
+        return "{stateCode:healthy}";
     }
 }
